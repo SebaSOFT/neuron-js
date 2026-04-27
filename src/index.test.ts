45d937f8b6bbd3fbb97c5aaf5f88fca33baebdd9
Sebastian Dieguez <s.dieguez@wotc.com>
2026-04-27 19:20:57 -0300

    feat: implement modernized neuron registry

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,3 +5,10 @@ test("Neuron can be instantiated", () => {
   const neuron = new Neuron();
   expect(neuron).toBeDefined();
 });
+
+test("Neuron can register and retrieve types", () => {
+  const neuron = new Neuron();
+  class MockAction {}
+  neuron.registerAction("mock_action", MockAction);
+  expect(neuron.getAction("mock_action")).toBe(MockAction);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-export class Neuron {}
+export type Constructor<T> = new (...args: any[]) => T;
+
+export class Neuron {
+  private registries = {
+    parameters: new Map<string, Constructor<any>>(),
+    conditions: new Map<string, Constructor<any>>(),
+    actions: new Map<string, Constructor<any>>(),
+    rules: new Map<string, Constructor<any>>(),
+  };
+
+  registerParameter(type: string, ctor: Constructor<any>) {
+    this.registries.parameters.set(type, ctor);
+  }
+
+  getParameter(type: string) {
+    return this.registries.parameters.get(type);
+  }
+
+  registerCondition(type: string, ctor: Constructor<any>) {
+    this.registries.conditions.set(type, ctor);
+  }
+
+  getCondition(type: string) {
+    return this.registries.conditions.get(type);
+  }
+
+  registerAction(type: string, ctor: Constructor<any>) {
+    this.registries.actions.set(type, ctor);
+  }
+
+  getAction(type: string) {
+    return this.registries.actions.get(type);
+  }
+
+  registerRule(type: string, ctor: Constructor<any>) {
+    this.registries.rules.set(type, ctor);
+  }
+
+  getRule(type: string) {
+    return this.registries.rules.get(type);
+  }
+}
