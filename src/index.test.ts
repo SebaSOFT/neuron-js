d3b63315638472dc99c53ac81a09d0bd572ba824
Sebastian Dieguez <s.dieguez@wotc.com>
2026-04-27 18:42:54 -0300

    feat: init neuron class with passing test

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest';
+import { Neuron } from './index.js';
+
+test('Neuron can be instantiated', () => {
+  const neuron = new Neuron();
+  expect(neuron).toBeDefined();
+});
